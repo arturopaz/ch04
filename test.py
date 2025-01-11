3a37bb79b69cae2074193a3c26e0551924d70b64
Arturo Paz <arturopazp@gmail.com>
2025-01-11 15:23:01 -0500

    Se modifico el archivo test.py

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-videogames = ['resident evil','metal gear','parasite eve','dino crisis']
-
-for videogame in videogames:
-    print(f"{videogame.title()} fue un gran videojuego.")
-    print(f"Me gustaria poder jugar el remake de {videogame.title()}\n")
-
-print("Estos fueron los mejores juegos que jugué en mi adolescencia.")
-
-
+my_comida = ['arroz chaufa','arroz con pollo','arroz con chancho','aji de gallina']
+comida_amigo = my_comida[:]
+print(type(my_comida), id(my_comida))
+print(type(comida_amigo), id(comida_amigo))
+print('Agrego comida a mi lista...')
+my_comida.append('lomito saltado')
+print('Agrego comida a la lista de mi amig..')
+comida_amigo.append('papa rellena')
+print('Mostrando las nuevas listas:\n')
+print(f"Mi lista: {my_comida}")
+print(f"Amigo lista: {comida_amigo}")
